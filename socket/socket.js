a6f6e2ccc7021d768364dda2df7b241c8f913a30
Dhiraj Shah <shahdhiraj0002@gmail.com>
2025-09-30 21:18:59 +0530

    m

--- socket/socket.js
+++ socket/socket.js
@@ -9,44 +9,30 @@ const io = new Server(server, {
   cors: {
     origin: ["https://chatfussion.netlify.app", "http://localhost:3000"],
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
+const userSocketMap = {};
 
-const userSocketMap = {}; // {userId: socketId}
+const getReceiverSocketId = (receiverId) => userSocketMap[receiverId];
 
-// Function to get the socket ID of a receiver
-const getReceiverSocketId = (receiverId) => {
-  return userSocketMap[receiverId];
-};
-
-// Socket connection event
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
-  const userId = socket.handshake.query.userId;  // Get userId from query
-
+  const userId = socket.handshake.auth.userId;  // Updated for Socket.IO v4+
   if (userId) {
-    userSocketMap[userId] = socket.id;  // Store the socket ID for the user
+    userSocketMap[userId] = socket.id;
     console.log(`User ${userId} connected with socket ID: ${socket.id}`);
   }
 
-  // Emit online users list
   io.emit("getOnlineUser", Object.keys(userSocketMap));
 
-  // Handle disconnect event
   socket.on("disconnect", () => {
     console.log("a user disconnected", socket.id);
-
-    if (userId) {
-      delete userSocketMap[userId];  // Remove the user from the map
-      console.log(`User ${userId} disconnected`);
-    }
-
-    // Emit updated online users list
+    if (userId) delete userSocketMap[userId];
     io.emit("getOnlineUser", Object.keys(userSocketMap));
   });
 });
 
-// Exporting app, io, and server to use in other modules
-module.exports = { app, io, server,getReceiverSocketId };
+module.exports = { app, io, server, getReceiverSocketId };
